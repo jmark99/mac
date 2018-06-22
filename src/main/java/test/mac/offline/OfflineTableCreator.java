c044793d76df3a14758e61126a9e6308add7b8cf
Mark Owens <jmark99@apache.org>
2018-06-22 15:13:45 -0400

    Updated main class to better monitor time taken to finish writing metadata on creation of offline tables. Refactored to make reading code a bit easier as well

--- src/main/java/test/mac/offline/OfflineTableCreator.java
+++ src/main/java/test/mac/offline/OfflineTableCreator.java
@@ -38,8 +38,7 @@ public class OfflineTableCreator {
   public void createOfflineTable()
       throws TableExistsException, AccumuloSecurityException, AccumuloException {
 
-    MiniUtils.pause("Started miniCluster with " + mac.getConfig().getNumTservers() + " "
-        + "tservers...");
+    MiniUtils.msg("Started miniCluster with " + mac.getConfig().getNumTservers() + " " + "tservers...");
 
     Properties props = MiniUtils.getProps();
     Connector conn = Connector.builder().usingProperties(props).build();
@@ -47,13 +46,12 @@ public class OfflineTableCreator {
     MiniUtils.msg("Splitfile : " + splitfile);
 
     createTable(conn, "onlineTable", new NewTableConfiguration());
-    MiniUtils.printTableIdInfo(conn);
-    MiniUtils.pause("Created onlineTable");
+    //MiniUtils.printTableIdInfo(conn);
+    MiniUtils.msg("Created onlineTable");
 
     createTable(conn, "offlineTable", new NewTableConfiguration().createOffline());
-    MiniUtils.printTableIdInfo(conn);
-    MiniUtils.pause("Created offlineTable");
-
+    //MiniUtils.printTableIdInfo(conn);
+    MiniUtils.msg("Created offlineTable");
 
     // Read a file of splits and place them into a collection that will be passed to
     // newTableCreation
@@ -68,28 +66,28 @@ public class OfflineTableCreator {
     } catch (IOException e) {
       e.printStackTrace();
     }
+    MiniUtils.msg("Number of splits to add: " + splits.size());
 
-    createTable(conn, "splitTableOffline", new NewTableConfiguration().createOffline()
-        .withSplits(splits));
-    MiniUtils.printTableIdInfo(conn);
-    MiniUtils.pause("Created splitTableOffline");
+    createTable(conn, "splitTableOffline", new NewTableConfiguration().createOffline().withSplits(splits));
+    //MiniUtils.printTableIdInfo(conn);
+    MiniUtils.msg("Created splitTableOffline");
 
-//    try {
-//      MiniUtils.msg("call online for table...");
-//      Stopwatch timer = Stopwatch.createUnstarted();
-//      timer.start();
-//      conn.tableOperations().online("splitTableOffline", true);
-//      timer.stop();
-//      MiniUtils.msg("splitTableOffline took " + timer + " to come online");
-//    } catch (TableNotFoundException e) {
-//      e.printStackTrace();
-//    }
-//    MiniUtils.pause("table now online..");
+    try {
+      MiniUtils.msg("call online for table...");
+      Stopwatch timer = Stopwatch.createUnstarted();
+      timer.start();
+      conn.tableOperations().online("splitTableOffline", true);
+      timer.stop();
+      MiniUtils.msg("splitTableOffline took " + timer + " to come online");
+    } catch (TableNotFoundException e) {
+      e.printStackTrace();
+    }
+    MiniUtils.msg("table now online..");
 
     createTable(conn, "splitTableOnline", new NewTableConfiguration().
         withSplits(splits), true);
-    MiniUtils.printTableIdInfo(conn);
-    MiniUtils.pause("Created splitTableOnline");
+    //MiniUtils.printTableIdInfo(conn);
+    MiniUtils.msg("Created splitTableOnline");
 
     MiniUtils.pause("exiting..");
   }
@@ -100,45 +98,60 @@ public class OfflineTableCreator {
   }
 
   public void createTable(Connector conn, final String tableName, NewTableConfiguration ntc,
-      boolean timeit)
-      throws AccumuloSecurityException, AccumuloException, TableExistsException {
+      boolean timeit) throws AccumuloSecurityException, AccumuloException, TableExistsException {
     MiniUtils.msg("create table " + tableName);
     Stopwatch timer = Stopwatch.createUnstarted();
     timer.start();
     conn.tableOperations().create(tableName, ntc);
-    if (timeit) {
-      Map<String,String> idMap = conn.tableOperations().tableIdMap();
-      String tableId = idMap.get(tableName);
-      MiniUtils.msg("TableId = " + tableId);
-      boolean hasBeenNonZero = false;
-      timer.stop();
-      MiniUtils.msg("Took " + timer + " to write metadata");
-      timer.start();
-      int i = 0;
-      while(true) {
-        int fcnt = 0;
-        try (Scanner scan = conn.createScanner("accumulo.metadata", Authorizations.EMPTY)) {
-          scan.setRange(Range.prefix(tableId));
-          scan.fetchColumnFamily(new Text("future"));
-          for(Map.Entry<Key,Value> entry : scan) {
-            fcnt++;
-          }
-          MiniUtils.msg("FutureCount: " + fcnt);
-        } catch (TableNotFoundException e) {
-          e.printStackTrace();
-        }
-        if (fcnt > 0) hasBeenNonZero = true;
-        if (fcnt == 0 && hasBeenNonZero == true) {
-          break;
-        }
-        i++;
-        if  (fcnt > 15000)
-          MiniUtils.sleep(60000);
-        else
-          MiniUtils.sleep(10000);
+    timer.stop();
+    MiniUtils.msg("Call to create returned in " + timer);
+    if (timeit)
+      timeMetadataCreation(conn, tableName, ntc.getSplits().size(), timer);
+  }
+
+  private void timeMetadataCreation(Connector conn, String tableName, int splitSize, Stopwatch timer) {
+    timer.start();
+    int futureCnt = 0; int locCnt = 0;
+    Scanner fScanner = null; Scanner lScanner = null;
+    String tableId = getTableId(conn, tableName);
+    int pCnt = 0;
+    while (locCnt < splitSize) {
+      try {
+        lScanner = getColumnScammer(conn, tableId, "loc");
+        locCnt = getEntryCount(lScanner);
+      } finally {
+        lScanner.close();
       }
+      if (pCnt++ % 6 == 0)
+        MiniUtils.msg("loc : " +  locCnt);
+      MiniUtils.sleep(5000);
     }
     timer.stop();
-    MiniUtils.msg("Creation of " + tableName + " took " + timer);
+    MiniUtils.msg("Metadata updates for " + tableName + " completed in " + timer);
+  }
+
+  private Scanner getColumnScammer(Connector conn, String tableId, String column) {
+    Scanner scanner = null;
+    try {
+      scanner = conn.createScanner("accumulo.metadata", Authorizations.EMPTY);
+      scanner.setRange(Range.prefix(tableId));
+      scanner.fetchColumnFamily(new Text(column));
+    } catch (TableNotFoundException e) {
+      MiniUtils.msg("TableNotFoundException: " + e.getMessage());
+    }
+    return scanner;
+  }
+
+  private int getEntryCount(Scanner scanner) {
+    int cnt = 0;
+    for(Map.Entry<Key,Value> entry : scanner)
+      cnt++;
+    return cnt;
+  }
+
+  private String getTableId(Connector conn, String tableName) {
+    Map<String,String> idMap = conn.tableOperations().tableIdMap();
+    String tableId = idMap.get(tableName);
+    return tableId;
   }
 }
